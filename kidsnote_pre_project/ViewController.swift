454580f6b52184ab1d542fffd9c78250fe02eab7
방용식 <lovelionet@gmail.com>
2024-09-21 15:23:42 +0900

    초기 설정

---
+++ kidsnote_pre_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kidsnote_pre_project
+//
+//  Created by 방용식/대리개발 on 9/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ kidsnote_pre_projectUITests/kidsnote_pre_projectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  kidsnote_pre_projectUITestsLaunchTests.swift
+//  kidsnote_pre_projectUITests
+//
+//  Created by 방용식/대리개발 on 9/21/24.
+//
+
+import XCTest
+
+final class kidsnote_pre_projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
